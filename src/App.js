12d5e84ad3499cadcdd361089bfe8b4440f74b1c
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-11-04 15:05:08 -0700

    added localStorage hook to store cart in local storage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from "./contexts/ProductContext"
 import CartContext from "./contexts/CartContext";
+import useLocalStorage from "./hooks/useLocalStorage"
 
 // Components
 import Navigation from './components/Navigation';
@@ -11,14 +12,14 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useLocalStorage('cart',[]);
 
 	const addItem = item => {
 		setCart([...cart,item]);
 	};
 	const removeItem = id =>{
 		setCart(cart.filter((item)=>{
-			return item.id != id 
+			return item.id !== id 
 		}));
 	}
 
